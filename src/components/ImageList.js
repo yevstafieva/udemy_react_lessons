e69e9d74858f74c9ce858482bace6d3aec819027
Tamara Y <t.yevstafieva@gmail.com>
2019-06-12 22:30:55 +0200

    Building List of Records

---
+++ src/components/ImageList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ImageList = (props) => {
+    return (props.images.map(({description, id, urls} ) => {
+        return <img alt={description} key={id} src={urls.regular}/>
+    } ))
+}
+
+export default ImageList;
